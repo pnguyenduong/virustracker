6fa75c26f73095aaab2afa4f75047557077b4759
Phong Nguyen Duong <pnguyenduong@gmail.com>
2020-04-18 13:42:15 -0500

    change database

--- configs/settings.py
+++ configs/settings.py
@@ -7,9 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
 
     # database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
-        'sqlite:///' + os.path.join(basedir, 'temp.db')
-        
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     # app conflicts in console for SQLALCHEMY_TRACK_MODIFICATIONS
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
