686f00509bb2692374fc9b44854c912164867f9f
Phong Nguyen Duong <pnguyenduong@gmail.com>
2020-04-24 23:40:31 -0500

    change data to integer value

--- app/models.py
+++ app/models.py
@@ -4,18 +4,16 @@ from app import db
 
 class VirusData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    case_total = db.Column(db.String())
-    case_today = db.Column(db.String())
-    case_active = db.Column(db.String())
-    case_serious = db.Column(db.String())
-    recovered_total = db.Column(db.String())
-    death_today = db.Column(db.String())
-    death_total = db.Column(db.String())
+    name = db.Column(db.String)
+    case_total = db.Column(db.Integer)
+    case_today = db.Column(db.Integer)
+    case_active = db.Column(db.Integer)
+    case_serious = db.Column(db.Integer)
+    recovered_total = db.Column(db.Integer)
+    death_today = db.Column(db.Integer)
+    death_total = db.Column(db.Integer)
     date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'ID:{self.id}, Country: {self.name}, Total Cases: {self.case_total}, Today Cases: {self.case_today}, \
-                Active Case: {self.case_active}, Serious: {self.case_serious}, Recovered: {self.recovered_total}, \
-                Today Deaths: {self.death_today}, Total Deaths: {self.death_total}, Date: {self.date}'
+        return f'''ID:{self.id}, Country: {self.name}, Total Cases: {self.case_total}, Today Cases: {self.case_today},Active Case: {self.case_active}, Serious: {self.case_serious}, Recovered: {self.recovered_total},Today Deaths: {self.death_today}, Total Deaths: {self.death_total}, Date: {self.date}'\n'''
 
